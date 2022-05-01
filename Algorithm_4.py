ac86f724a253ee71be4d751305f3c53d5b856b87
jjonpark <rupinjs@gmail.com>
2022-05-01 19:11:56 +0900

    220501

--- Algorithm_4.py
+++ Algorithm_4.py
@@ -77,35 +77,139 @@
     # b_list.sort()
 
 #백준 문제 < 개똥 벌레 >  풀이 
-def lower_bound(s,e,d,l):
-    while (e-s>0):
-        m=(s+e)//2
-        if(l[m]<d):
-            s=m+1
-        else:
-            e=m
-    return e
-up=[]
-down=[]
-result=[0]*500001
-n,h=map(int,input().split())
-for i in range(n):
-    obstacle=int(input())
-    if i%2==1:
-        up.append(obstacle)
-    else:
-        down.append(obstacle)
+    # def lower_bound(s,e,d,l):
+    #     while (e-s>0):
+    #         m=(s+e)//2
+    #         if(l[m]<d):
+    #             s=m+1
+    #         else:
+    #             e=m
+    #     return e
+    # up=[]
+    # down=[]
+    # result=[0]*500001
+    # n,h=map(int,input().split())
+    # for i in range(n):
+    #     obstacle=int(input())
+    #     if i%2==1:
+    #         up.append(obstacle)
+    #     else:
+    #         down.append(obstacle)
 
-up.sort()
-down.sort()
-answer=0
-mx=2147483647
-for i in range(1,h+1):
-    idxd=lower_bound(0,len(down),i,down)
-    idxu=lower_bound(0,len(up),h-i+1,up)
-    result[i]=n//2-idxd+n//2-idxu
-    mx=min(mx,result[i])
-for i in range(1,h+1):
-    if result[i]==mx:
-        answer+=1
-print(mx,answer)
+    # up.sort()
+    # down.sort()
+    # answer=0
+    # mx=2147483647
+    # for i in range(1,h+1):
+    #     idxd=lower_bound(0,len(down),i,down)
+    #     idxu=lower_bound(0,len(up),h-i+1,up)
+    #     result[i]=n//2-idxd+n//2-idxu
+    #     mx=min(mx,result[i])
+    # for i in range(1,h+1):
+    #     if result[i]==mx:
+    #         answer+=1
+    # print(mx,answer)
+
+
+#백준 문제 <수 정렬하기>
+    # M=int(input())
+    # array=[]
+    # for i in range(M):
+    #     k=int(input())
+    #     array.append(k)
+    # array.sort()
+    # for i in range(M):
+    #     print(array[i])
+
+#백준 문제 <저울> -> 저번에 시험본 그 유형이랑 좀 비슷한거 같다. 풀이를 보고 빠져보자
+    # import itertools
+    # N=int(input())
+    # array=list(map(int,input().split()))
+    # array.sort()
+    # print(array)
+    # array=[1,2,3]
+    # combi=list(itertools.combinations(array,2))
+    # print(combi)
+
+#백준 문제 < 저울 > 풀이 -> 특이한..방법이네?
+    # N=int(input())
+    # array=list(map(int,input().split()))
+    # array.sort()
+    # answer=1
+    # for i in range(N):
+    #     if answer>=array[i]:
+    #         answer+=array[i]
+    #     else: 
+    #         break
+    # print(answer)
+
+#백준 문제 < 계수 정렬 > 
+    # import sys
+    # n=int(input())
+    # count_sort=[0]*10001
+    # for i in range(n):
+    #     count_sort[int(sys.stdin.readline())]+=1
+    # for i in range(10001):
+    #     if count_sort[i]!=0:
+    #         for _ in range(count_sort[i]):
+    #             print(i)
+
+#백준 문제 < 나이순 정렬 >
+    # N=int(input())
+    # array=[]
+    # for i in range(N):
+    #     k_1,k_2=input().split()
+    #     k_1=int(k_1)
+    #     array.append([k_1,k_2])
+
+    # answer=sorted(array,key=lambda x:x[0])
+
+    # for i in range(N):
+    #     print(f"{answer[i][0]} {answer[i][1]}")
+
+#백준 문제 <숫자의 합>
+    # n=int(input())
+
+    # x=list(str(input()))
+    # answer=0
+    # for i in range(n):
+    #     answer+=(ord(x[i])-48)    
+    # print(answer)
+
+#백준 문제 < 백대열 >
+    # a,b=map(int,input().split(':'))
+
+    # def GCD(a,b):
+    #     if b%a:
+    #         return GCD(b%a,a)
+    #     else:
+    #         return a
+
+    # k=GCD(a,b)
+    # a_1=a//k
+    # b_1=b//k
+    # print(f"{a_1}:{b_1}")
+
+#백준 문제 < 문자열 폭발 > ->하나씩 보면서 스택의 개념으로 지워주는
+s=str(input())
+bomb=str(input())
+left=[]
+start=0
+end=len(s)-1
+while start<=end:
+    tof=True
+    left.append(s[start])
+    start+=1
+    if(len(left)>=len(bomb)):
+        for i in range(len(bomb)):
+            if bomb[i]!=left[len(left)-len(bomb)+i]:
+                tof=False
+                break
+        if tof==True:
+            for i in range(len(bomb)):
+                left.pop()
+if len(left)==0:
+    print("FRULA")
+else:
+    for i in range(len(left)):
+        print(left[i], end='')
